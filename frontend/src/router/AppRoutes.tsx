27aaf76da2bf5e289b12d3f4be1270a16237af8a
yohan <yohan@ubuntu22.myguest.virtualbox.org>
2025-07-25 16:34:16 -0500

    feat(error): agregar página de error 404 y enrutamiento

--- frontend/src/router/AppRoutes.tsx
+++ frontend/src/router/AppRoutes.tsx
@@ -1,12 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import InitialSetup from '../pages/InitialSetup'
+import ProtectedRoute from './ProtectecRoute'
+import NotFound from '../pages/NotFound'
 
-export default function AppRoutes() {
+function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/initial-setup" element={<InitialSetup />} />
+
+      <Route
+        element={<ProtectedRoute><InitialSetup /></ProtectedRoute>}
+      >
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
+
+export default AppRoutes
