9394c684c7c6a19e89542c3709289cf417dc846f
yohanvillarp <yohannikel21@gmail.com>
2025-08-20 15:39:07 -0500

    Agrega envio de datos en tiempo real y corrige creación de servicio automatico

--- backend/src/ws/webSocketServer.ts
+++ backend/src/ws/webSocketServer.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from 'ws';
 import type { Server } from 'http';
 import { handleMessage } from './wsMessageHandler';
-
+import { spawn } from 'child_process';
 
 export function createWebSocketServer(server: Server) {
     const wss = new WebSocketServer({ server });
@@ -16,20 +16,53 @@ export function createWebSocketServer(server: Server) {
         });
     });
 
-    // Intervalo de 5 segundos para enviar métricas
+    const scriptPathSystem = 'get/get_system_info.sh';
+    const scriptPathSuricata = 'get/get_suricata_logs.sh';
     const interval = 5000;
+
+    // 1️Script del sistema cada 5 segundos
     setInterval(() => {
         const messageToSend = JSON.stringify({
             type: 'exec-script',
-            payload: {
-                script: "get/get_system_info.sh",
-            }
+            payload: { script: scriptPathSystem }
         });
-        // Enviar a todos los clientes conectados
-        wss.clients.forEach((client) => {
+
+        wss.clients.forEach(client => {
             if (client.readyState === 1) {
                 handleMessage(client, Buffer.from(messageToSend));
             }
         });
     }, interval);
+
+    // Script de Suricata en tiempo real, compartido para todos los clientes
+    const suricataScript = spawn('/bin/bash', [`scripts/${scriptPathSuricata}`]);
+
+    suricataScript.stdout.on('data', (data) => {
+        const lines = data.toString().split('\n').filter(Boolean);
+        console.log("hay nuevos datos");
+        lines.forEach((line: string) => {
+            const messageToSend = JSON.stringify({
+                type: 'exec-script',
+                payload: {
+                    script: scriptPathSuricata,
+                    output: line
+                }
+            });
+
+            // Enviar a todos los clientes conectados
+            wss.clients.forEach(client => {
+                if (client.readyState === 1) {
+                    handleMessage(client, Buffer.from(messageToSend), true);
+                }
+            });
+        });
+    });
+
+    suricataScript.stderr.on('data', (err) => {
+        console.error('Error ejecutando script Suricata:', err.toString());
+    });
+
+    suricataScript.on('close', (code) => {
+        console.log(`Script Suricata finalizó con código ${code}`);
+    });
 }
